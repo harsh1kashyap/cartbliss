982316e2e0a3f5e32d97b18c6cf65153f0453978
Harsh Kashyap <harshvardhanjha4@gmail.com>
2025-02-14 09:27:00 +0530

    login backend code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import Login from './pages/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
     <>
-      <div>
-        <Login/>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/Home' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,63 @@
+import { useState } from "react";
+import axios from "axios";
+import { ToastContainer, toast } from 'react-toastify';
 import loginImg from "../assets/login_img_1.png";
 import '../styles/Login.css'
 
+
 var Login = () => {
+
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('https://localhost:5001/api/UserLoginRegister/VerifyLogin', { userName: userName, password: password })
+            .then(function (response) {
+                toast.success("Logged In Successfully.");
+            })
+            .catch(function (response) {
+                console.log(response);
+            })
+    }
+
     return (
         <>
-            <div className="row">
+            <div className="row vh-100 d-flex align-items-center">
                 <div className="col-md-6 main_div d-flex justify-content-center">
-                    <img src={loginImg} className="login_img"></img>
+                    <img src={loginImg} className="login_img" alt="Login" />
                 </div>
-                <div className="col-md-6">
-                    <div className="card p-4 shadow-lg login_form">
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                    <div className="p-3">
                         <h4>Sign In</h4>
-                        <p>Don’t have an accout yet? <span>Sign Up</span></p>
-                        <div>
-                            <form>
-                                <div className="mb-3">
-                                    <label className="form-label">User Name</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        placeholder="Enter Username"
-                                        required
-                                    />
-                                </div>
-                                <div className="mb-3">
-                                    <label className="form-label">Password</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        placeholder="Enter password"
-                                        required
-                                    />
-                                </div>
-                                <button type="submit" className="btn btn-primary w-100">
-                                    Sign In
-                                </button>
-                            </form>
-                        </div>
+                        <p>
+                            Don’t have an account yet? <span className="text-primary">Sign Up</span>
+                        </p>
+                        <form onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label className="form-label">User Name</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter Username"
+                                    required
+                                    onChange={(e) => setUserName(e.target.value)}
+                                />
+                            </div>
+                            <div className="mb-3">
+                                <label className="form-label">Password</label>
+                                <input
+                                    type="password"
+                                    className="form-control"
+                                    placeholder="Enter password"
+                                    required
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary w-100">
+                                Sign In
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
